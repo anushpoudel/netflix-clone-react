8d60977bf191539964f143f7dc2d47afe2c6776a
Anush Poudel <apoudel55@gmail.com>
2021-03-24 16:15:34 +1100

    fetched data from api

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from "react";
+import "./Row.css";
 import axios from "./axios";
 
+//https://www.youtube.com/watch?v=XtMThy8QKqU&t=1s
+//paused at 1:12:44
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
