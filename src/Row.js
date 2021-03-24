028bd45b2cfc0962d7c9e2052c2236b306390463
Anush Poudel <apoudel55@gmail.com>
2021-03-24 15:03:31 +1100

    initial setup

---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+}
+
+export default Row;
---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "12ce07f36043796250420c6fd2de3425";
+
+// Object for various requests for movies
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+// Exporting request object. Making it available
+export default requests;
