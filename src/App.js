5b75aad993ef68d3318b1c092643a10b95ced30f
Anush Poudel <apoudel55@gmail.com>
2021-03-28 01:51:14 +1100

    testing firebase deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Nav />
       <Banner />
       <Row
-        title="NETFLIX ORIGINALS"
+        title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
