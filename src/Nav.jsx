916ce851a1a9f8caa45fb0557904fa330444f3fe
Anush Poudel <apoudel55@gmail.com>
2021-03-27 23:20:22 +1100

    final version

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
-const Nav = () => {
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  // When scroll is a 100px (down in page) we add the navbar visibilty
+  // if not remove visibility on navbar
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
+      {/* Big main logo */}
       <img
         className="nav_logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Netflix_2014_logo.svg"
-        alt="Netflix Logo"
+        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+        alt=""
+      />
+      {/* Small corner logo */}
+      <img
+        className="nav_avatar"
+        src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+        alt=""
       />
     </div>
   );
-};
+}
 
 export default Nav;
--- src/Row.jsx
+++ src/Row.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "./axios";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 //https://www.youtube.com/watch?v=XtMThy8QKqU&t=1s
 //paused at 1:56:22
@@ -8,6 +10,7 @@ import axios from "./axios";
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -18,7 +21,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.table(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      // Search for movie trailer full url
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=aSØDÆømlsdæ
+          const urlParams = new URLSearchParams(new URL(url).search); // urlParams gives us everthing after the ?
+          setTrailerUrl(urlParams.get("v")); //urlParams gives us everything after v=
+          // Displays error message if unable to find url
+        })
+        .catch((error) => console.log(error));     
+    }
+  };
 
   return (
     <div className="row">
@@ -28,14 +53,17 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
     </div>
   );
 }
